59d049efa80f3f2aff7e2d870c6a739647ebacf4
ericyoung730 <ericyoung730@gmail.com>
2017-11-11 16:00:10 +0800

    Create animate.js

---
+++ animate.js
@@ -0,0 +1,31 @@
+var countdownnumber=3605;
+var countdownid,h,m,s,demo;
+
+function initial(){ 
+   
+  h=Math.floor((countdownnumber % (60 * 60 * 24)) / (60 * 60));
+   
+  m=Math.floor((countdownnumber % ( 60 * 60)) / (60));
+  
+  s=Math.floor((countdownnumber % 60));
+  demo=document.getElementById("demo");
+  demo.innerHTML =  h + "h "+ m + "m " + s + "s ";
+  countdownnumber--;
+  countdownid=window.setInterval(countdownfunc,1000);
+}
+function countdownfunc(){ 
+ h=Math.floor((countdownnumber % (60 * 60 * 24)) / (60 * 60));
+   
+  m=Math.floor((countdownnumber % ( 60 * 60)) / (60));
+  
+  s=Math.floor((countdownnumber % 60));
+   demo.innerHTML =  h + "h " + m + "m " + s + "s ";
+  if (countdownnumber==0){
+    alert("倒數結束");
+    clearInterval(countdownid);
+  }
+  countdownnumber--;
+}
+function myFunction() {
+    document.getElementById("myForm").submit();
+}
